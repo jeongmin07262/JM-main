accefdf91b4b012bf31a61d51c38d4f32c57be6c
jeongmin <dlwjdals0726@gmail.com>
2024-05-20 22:26:12 +0900

    Changes to be committed: 	new file:   SNS-PROJECT/board-front/src/components/Pagination/index.tsx 	new file:   SNS-PROJECT/board-front/src/components/Pagination/style.css 	modified:   SNS-PROJECT/board-front/src/mocks/favorite-list.mock.ts 	modified:   SNS-PROJECT/board-front/src/views/Board/Detail/index.tsx

--- SNS-PROJECT/board-front/src/mocks/favorite-list.mock.ts
+++ SNS-PROJECT/board-front/src/mocks/favorite-list.mock.ts
@@ -23,6 +23,11 @@ const favoriteListMock = [
         "email": "email@email.com",
         "nickname": "안녕하세요나는주코야키",
         "profileImage": null
+    },
+    {
+        "email": "email@email.com",
+        "nickname": "안녕하세요나는주코야키",
+        "profileImage": null
     }
 ];
 
