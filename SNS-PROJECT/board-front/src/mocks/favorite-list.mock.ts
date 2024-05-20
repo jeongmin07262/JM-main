fd4d8bd8ac62be55711e536a26346e0cb3077265
jeongmin <dlwjdals0726@gmail.com>
2024-05-20 23:48:06 +0900

    Changes to be committed: 	modified:   SNS-PROJECT/board-front/src/App.css 	new file:   SNS-PROJECT/board-front/src/assets/image/chat-light.png 	modified:   SNS-PROJECT/board-front/src/assets/image/default-profile-image.png 	new file:   SNS-PROJECT/board-front/src/assets/image/expand-down-light.png 	new file:   SNS-PROJECT/board-front/src/assets/image/expand-left.png 	new file:   SNS-PROJECT/board-front/src/assets/image/expand-up-light.png 	new file:   SNS-PROJECT/board-front/src/assets/image/favorite-fill.png 	new file:   SNS-PROJECT/board-front/src/assets/image/favorite-light.png 	modified:   SNS-PROJECT/board-front/src/mocks/comment-list.mock.ts 	modified:   SNS-PROJECT/board-front/src/mocks/favorite-list.mock.ts 	modified:   SNS-PROJECT/board-front/src/views/Board/Detail/index.tsx 	modified:   SNS-PROJECT/board-front/src/views/Board/Detail/style.css

--- SNS-PROJECT/board-front/src/mocks/favorite-list.mock.ts
+++ SNS-PROJECT/board-front/src/mocks/favorite-list.mock.ts
@@ -1,32 +1,32 @@
 const favoriteListMock = [
     {
         "email": "email@email.com",
-        "nickname": "안녕하세요나는주코야키",
+        "nickname": "안녕나는이정민",
         "profileImage": null
     },
     {
         "email": "email@email.com",
-        "nickname": "안녕하세요나는주코야키",
+        "nickname": "안녕나는이정민",
         "profileImage": null
     },
     {
         "email": "email@email.com",
-        "nickname": "안녕하세요나는주코야키",
+        "nickname": "안녕나는이정민",
         "profileImage": null
     },
     {
         "email": "email@email.com",
-        "nickname": "안녕하세요나는주코야키",
+        "nickname": "안녕나는이정민",
         "profileImage": null
     },
     {
         "email": "email@email.com",
-        "nickname": "안녕하세요나는주코야키",
+        "nickname": "안녕나는이정민",
         "profileImage": null
     },
     {
         "email": "email@email.com",
-        "nickname": "안녕하세요나는주코야키",
+        "nickname": "안녕나는이정민",
         "profileImage": null
     }
 ];
